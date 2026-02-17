caae2bd8dde1e1024cc071cee6b4966df70650de
Gopal Jha <gopaljha9398715741@gmail.com>
2026-02-17 08:51:49 +0530

    Db issue fixed

--- packages/db/src/db.ts
+++ packages/db/src/db.ts
@@ -2,7 +2,10 @@ import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = process.env.DATABASE_URL;
+if (!connectionString) {
+    throw new Error("DATABASE_URL is not set");
+}
 
 const adapter = new PrismaPg({ connectionString });
 export const client = new PrismaClient({ adapter });
