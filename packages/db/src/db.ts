b3af469c1581e27620a73418faf96203413c0cc4
Mehul Prajapati <ailearn90@gmail.com>
2026-02-17 20:36:30 +0530

    fix: fixed env location

--- packages/db/src/db.ts
+++ packages/db/src/db.ts
@@ -1,8 +1,8 @@
-import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
+import { env } from "./config/env";
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = env.DATABASE_URL;
 if (!connectionString) {
     throw new Error("DATABASE_URL is not set");
 }
